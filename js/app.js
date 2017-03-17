5a14ad899038cc5822c7a4d5028887e770fd0655
Jeff Flower <jeffrey.flower@vantiv.com>
2017-03-17 16:32:02 -0600

    initial commit

---
+++ js/app.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(function() {
+  console.log('app js: ready');
+  var eventHandler = function (e) {
+    "use strict";
+    var response = JSON.parse(e.data);
+    console.log("custom callback");
+    console.log(response);
+  }
+  
+  function payframeClientCallback(res) {
+    console.log("payframe client callback: message received");
+    console.log(res);
+  }
+
+  var configure = {
+    paypageId: "myregistrationid",
+    style: "test",
+    reportGroup: "group1",
+    timeout: "5000",
+    div: "payframe",
+    callback: payframeClientCallback,
+    showCvv: true,
+    numYears: 8,
+    tabIndex: {
+      cvv: 4,
+      accountNumber: 1,
+      expMonth: 2,
+      expYear: 3
+    },
+  };
+
+  var payframeClient = new LitlePayframeClient(configure);
+
+  setTimeout(payframeClient.autoAdjustHeight, 100);
+});
